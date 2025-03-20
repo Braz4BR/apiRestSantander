54704f8355961d5b8b885a903f3094ec0f57b542
davib <davib@DESKTOP-K41716P>
2025-03-20 16:30:47 -0300

    Primeiro commit

---
+++ santander-dev-exemple-2025/src/main/java/exemple/first/santander_dev_exemple_2025/Application.java
@@ -0,0 +1,13 @@
+package exemple.first.santander_dev_exemple_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ santander-dev-exemple-2025/src/test/java/exemple/first/santander_dev_exemple_2025/ApplicationTests.java
@@ -0,0 +1,13 @@
+package exemple.first.santander_dev_exemple_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
